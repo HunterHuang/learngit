696f12ae973711b9675e5569471dbab8e913332d
HunterHuang <hunterhuang1470@163.com>
2018-05-30 20:59:12 +0800

    add Main java

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+class Main{
+
+	private int number;
+ 
+
+	public void triggerMe() {
+
+		String str = "First Me";
+
+		str.append("Second Me");
+
+		return;
+	}
+
+
+	public void scan() {
+
+		String str = "Please scan me !";
+
+		System.out.println(str);
+
+	}
+
+
+}
