bfc63ac5d26afd7ea4fbb4aeb97b182289a96658
hunterhuang1470@163.com <hunterhuang1470@163.com>
2018-05-30 22:57:32 +0800

    rebase testing commit by Master commit 1

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 class Main{
 
 
+	private int fuck_my_masetr
 	private int cus;
 	private int number;
  	
@@ -14,20 +15,20 @@ class Main{
 	}
 
 
-	public void scan() {
+	public void s2can() {
 
 		System.out.println(str);
 
 		System.out.println("Yeah")
 	}
 
-	public void addByMaster22() {
+	public void addByMaste2r22() {
 		System.out.println("Add b222y Master!")
 		System.out.println("Shit by22 master!")
 	}
 
-	public void addByDev2() {
-		System.out.println("Add de111v !")
+	public void addByDev32() {
+		System.out.println("Add22 de111v !")
 		System.out.println("Shit !")
 
 	}
