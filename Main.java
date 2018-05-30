fac56be787a7eb0c52fcd264291aebd689671388
hunterhuang1470@163.com <hunterhuang1470@163.com>
2018-05-30 22:56:04 +0800

    change by dev for rebase testing

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 
 class Main{
 
-
 	private int fuck_my_masetr
 	private int cus;
 	private int number;
+
+	private int cud;
+	private int c33;
+	private int number2;
+
  	
 
 	public void triggerMe() {
@@ -22,13 +26,21 @@ class Main{
 		System.out.println("Yeah")
 	}
 
+
 	public void addByMaste2r22() {
+
+	public void addMaster3() {
+
 		System.out.println("Add b222y Master!")
 		System.out.println("Shit by22 master!")
 	}
 
+
 	public void addByDev32() {
 		System.out.println("Add22 de111v !")
+
+	public void changeByDev3() {
+		System.out.println("Add2 de111v !")
 		System.out.println("Shit !")
 
 	}
